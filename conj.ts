f42de85bb9514d8e25d2de0b6e95b11ebca928fc
Chen, Chien-ting <chenjt30@gmail.com>
2021-01-10 21:17:59 +0800

    version 0.1

--- conj.ts
+++ conj.ts
@@ -9,6 +9,14 @@ var type_of_conj1 = null;
 
 var typesOfConj1Rads = document.getElementsByName("types-of-conj1");
 
+
+// reset the dropdown list and the 1st conj opt div while refreshing the page.
+window.addEventListener('pageshow', function(event) {
+  conjNoList.value = "1";
+  conjOptDiv.style.display = "block";
+});
+
+
 for(var i = 0; i < typesOfConj1Rads.length; i++) {
   let radValue = (<HTMLInputElement> typesOfConj1Rads[i]).value;
   typesOfConj1Rads[i].addEventListener('change',  () => {
@@ -35,7 +43,7 @@ startConj.addEventListener('click', (event) => {
   var original_text = original.value;
   var conjstem_text = conjStem.value;
   if (emptyString(original_text) && emptyString(conjstem_text)){
-    alert("您輸入的是空字串，請重新輸入。")
+    alert("您輸入的是空字串或空白字元，請重新輸入。")
   }
   else{
     if (emptyString(conjstem_text)){
@@ -83,18 +91,19 @@ let first_conj =
 let second_conj =
 { "broad" :
   {
-  "pres" : ["ím", "ír", "íonn", "aimíd", "ann sibh", "aid", null, "tar"],
-  "past" : ["as", "ais", null, "amair", "abhair", "adar", null, "adh"],
-  "pa_ha" : ["ainn", "tá", "adh", "aimís", "adh sibh", "aidís", "taí"],
-  "futu" : ["fad", "fair", "faidh", "faimíd", "faidh sibh", "faid", "far"],
-  "cond" : ["fainn", "fá", "fadh", "faimís", "fadh sibh", "faidís", "faí"],
-  "pr_su" : ["ad", "air", "aidh", "aimíd", "aidh sibh", "aid", null],
-  "impe" : ["aim", null, "adh", "aimís", "aidh", "aidís", null],
+  "pres" : ["aím", "aír", "aíonn", "aímíd", "aíonn sibh", "aíd", null, "aítear"],
+  "past" : ["aíos", "aís", null, "aíomair", "aíobhair", "aíodar", null, "aíodh"],
+  "pa_ha" : ["aínn", "aíteá", "aíodh", "aímís", "aíodh sibh", "aídís", "aítí"],
+  "futu" : ["ód", "óir", "óidh", "óimíd", "óidh sibh", "óid", "ófar"],
+  "cond" : ["óinn", "ófá", "ódh", "óimís", "ódh sibh", "óidís", "ófaí"],
+  "pr_su" : ["aíod", "aír", "aídh", "aímíd", "aídh sibh", "aíd", null],
+  "impe" : ["aím", null, "aíodh", "aímís", "aídh", "aídís", null],
   },
   "slender" :
   {
     "pres" : ["ím", "ír", "íonn", "ímíd", "íonn sibh", "íd", null, "ítear"],
-    "pa_ha" : ["ínn", "íteá", "íodh", "ímís", "íodh sibh", "ídís", "ití"],
+    "past" : ["íos", "ís", null, "íomair", "íobhair", "íodar", null, "íodh"],
+    "pa_ha" : ["ínn", "íteá", "íodh", "ímís", "íodh sibh", "ídís", "ítí"],
     "futu" : ["eod", "eoir", "eoidh", "eoimíd", "eoidh sibh", "eoid", "eofar"],
     "cond" : ["eoinn", "eofá", "eodh", "eoimís", "eodh sibh", "eoidís", "eofaí"],
     "pr_su" : ["íod", "ír", "ídh", "ímíd", "ídh sibh", "íd", null],
@@ -152,9 +161,16 @@ function generateConj(orig_txt : string, stem_txt : string, conj_no : Number){
 
 
   //  Gael => ['G', 'ae', 'l']; ithim => [i, th]
- var stem_txt_splitted = stem_txt.split(/([aA][eE]|[bcdfgmpstBCDFGMPST][hH]|[A-Za-z])/).filter(text => text);
- var isBroadEnd = checkedIfBroadEnd(stem_txt_splitted);
- if (conj_no == 1){
+  function split_text(txt : string){
+    return txt.split(/([aA][eE]|[bcdfgmpstBCDFGMPST][hH]|[A-Za-z])/).filter(text => text);
+  }
+
+  var isBroadEnd;
+
+  if (conj_no == 1){
+
+    var stem_txt_splitted = split_text(stem_txt);
+    checkedIfBroadEnd(stem_txt_splitted);
 
     if (type_of_conj1 == "igh"){
       if (!orig_txt.match('[iI][gG][hH]$')){
@@ -226,19 +242,45 @@ function generateConj(orig_txt : string, stem_txt : string, conj_no : Number){
       }
     }
   
-    // general 1st vonj verbs
-  function firstConjFillTheArray() {
-      var suffixTable;
-      if (isBroadEnd) {
-        suffixTable = first_conj["broad"];
-      } else {
-        suffixTable = first_conj["slender"];
-      }
+  }
+  // conj_no == 2
+  else{
 
-      fillTablesInner(suffixTable);
-      
+    
+    // remove the -(a)igh of orig. text as the stem txt
+    if (orig_txt.match(/.*[aA]?[iI][gG][hH]$/)){
+      console.log("matched!");
+      stem_txt = /(.*?)[aA]?[iI][gG][hH]/.exec(orig_txt)[1];
+    }
+    console.log(stem_txt);
+
+    var stem_txt_splitted = split_text(stem_txt);
+    isBroadEnd = checkedIfBroadEnd(stem_txt_splitted);
+
+    var suffixTable;
+    if (isBroadEnd) {
+      suffixTable = second_conj["broad"];
+    } else {
+      suffixTable = second_conj["slender"];
+    }
+    
+    
+    
+    fillTablesInner(suffixTable);
+  }
+  
+
+  // general 1st vonj verbs
+  function firstConjFillTheArray() {
+    var suffixTable;
+    if (isBroadEnd) {
+      suffixTable = first_conj["broad"];
+    } else {
+      suffixTable = first_conj["slender"];
     }
 
+    fillTablesInner(suffixTable);
+  }
 
   function fillTablesInner(suffixTable : any) {
     var suffixTableKeys = Object.keys(suffixTable);
@@ -258,6 +300,6 @@ function generateConj(orig_txt : string, stem_txt : string, conj_no : Number){
         }
 
       }
-    }
   }
+
 }
